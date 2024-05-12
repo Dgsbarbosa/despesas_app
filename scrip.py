f9fdf2a465768e225ff3de84f154df1cd25c2254
DOUGLAS BARBOSA <guinho.cq@gmail.com>
2024-05-12 19:56:00 -0300

    funcoes

--- scrip.py
+++ scrip.py
@@ -9,8 +9,6 @@ import calendar
 meses = {1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 5: 'maio', 6: 'junho', 7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
 
 
-def main():
-    ...
 
 def ler_excel():
     with pd.ExcelFile("Contas Basicas.xlsx") as xlsx:
@@ -24,31 +22,18 @@ def ler_excel():
     return df_receitas, df_despesas
 
    
-
-def organiza_dados():
-        
-    receitas, despesas = ler_excel()
+def organiza_dados(df):     
     
-    dados_receitas = []
-    dados_despesas = []
     
-    for index, receita in receitas.iterrows():
+    dados = []
+    
+    for _, receita in df.iterrows():
         
         dado = {"nome": receita['nome'],"vencimento":receita["vencimento"],"valor":receita["valor"],"tipo":receita["tipo"],"parcelas":receita["parcelas"], "primeira parcela":receita["primeira parcela"]}
-        dados_receitas.append(dado)
+        dados.append(dado)
         
-    
-    
-    for index, despesa in despesas.iterrows():
-        
-        dado = {"nome": despesa["nome"], "vencimento": despesa["vencimento"],"valor":despesa["valor"],"tipo":despesa["tipo"],"parcelas":despesa["parcelas"], "primeira parcela":despesa["primeira parcela"]}  
-        dados_despesas.append(dado) 
- 
 
-        
-    # print("Receitas: ",dados_receitas,"Despesas: ", dados_despesas)
-
-    return {"receitas":dados_receitas, "despesas":dados_despesas}
+    return dados
 
 def calendario_do_ano():
     
@@ -56,13 +41,14 @@ def calendario_do_ano():
     ano = datetime.datetime.now().year
     
     global meses
-    # meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+    # meses = ['janeiro', 'fevereiro', 'marÃ§o', 'abril', 'maio', 'junho',
     #      'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'] 
     
-    dias_da_semana = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo']
+    dias_da_semana = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sabado', 'domingo']
     
     calendario = calendar.Calendar()
     
+   
     
     dias_do_ano = {}
     
@@ -93,54 +79,48 @@ def calendario_do_ano():
     
     return dias_do_ano
 
-def verifica_parcela():
+def verifica_parcela(receitas, despesas):
     
-    global meses
-    contas = organiza_dados()
+    global meses    
     
-    receitas = contas["receitas"]
-    despesas = contas["despesas"]
-
+    
     for receita in receitas:
         
+               
         if math.isnan(receita["parcelas"]) == False:
             
             qtd_parcelas = receita["parcelas"]
             mes_primeira_parcela = receita["primeira parcela"]
 
-            # print(qtd_parcelas)
-            # print(mes_primeira_parcela)           
-            # print(meses.get(1))
             
             for chave_mes, mes_do_ano in meses.items():
-                
+                                
                 if mes_do_ano == mes_primeira_parcela:
                     
-                    ultima_parcela = chave_mes + qtd_parcelas - 1
+                    ultima_parcela = int(chave_mes + qtd_parcelas - 1)
                     
+                    parcelas_excedentes = "" 
+                    if ultima_parcela > 12:
+                        
+                        parcelas_excedentes = ultima_parcela - 12
+                        ultima_parcela = 12
+                        
                     meses_parcela = []
                     
                     for mes in range(chave_mes,ultima_parcela + 1):
                         
                         meses_parcela.append(meses[mes])
                     
-                    receita["meses"] = meses_parcela
-                    # print(meses_parcela)
                     
-            print("receita")
-            print(receita)
-            
-            
-            
-            
+                    if parcelas_excedentes:
+                        meses_parcela.append(f" + {parcelas_excedentes}")
+                   
+                    receita["meses"] = meses_parcela
+                 
             
         else:
-            # print(receita["parcelas"])
-            # print("esta vazio")
             continue
         
-        # print(receita)
- 
  
     for despesa in despesas:
         
@@ -156,9 +136,12 @@ def verifica_parcela():
                     chave_mes = int(chave_mes)
                     ultima_parcela = int(chave_mes + qtd_parcelas - 1)
                     
+                    parcelas_excedentes = "" 
                     if ultima_parcela > 12:
-                        ultima_parcela = 12
-                    
+                        
+                        parcelas_excedentes = ultima_parcela - 12
+                        ultima_parcela = 12        
+                        
                     meses_parcela = []
                     
                     
@@ -166,28 +149,20 @@ def verifica_parcela():
                         
                         meses_parcela.append(meses[mes])
                     
+                    if parcelas_excedentes:
+                        meses_parcela.append(f" + {parcelas_excedentes}")
                     despesa["meses"] = meses_parcela
-                    # print(meses_parcela)
-                    
-            print("despesa")
-            print(despesa)
-            
-            
-            
-            
             
         else:
-            # print(receita["parcelas"])
-            # print("esta vazio")
             continue
-    # print(contas)
-    # print(receitas)
-    # print(despesas)
     
-    
+    return receitas, despesas
+          
 
 def verifica_vencimentos(mes):
     
+    
+
     contas = verifica_parcela()
     dias_do_ano = calendario_do_ano()
     
@@ -195,20 +170,119 @@ def verifica_vencimentos(mes):
     
     receitas = contas["receitas"]
     despesas = contas["despesas"]
-    
-    
-    
-    # print(contas)     
-    # print(dias_do_ano)
-    
-    # print(dias_do_mes)
-    # print(receitas)
-    
-    # print(despesas)
-    
-    # print(calendario)
 
+def linhas_planilha(receitas, despesas):
+    
+    calendario = calendario_do_ano()
+    
+    linhas = {}
+    for mes in calendario.keys():
+        linhas[mes] = ""
+        
+        for dia in calendario[mes]:
+            ...
+            for receita in receitas:
+                if dia['dia'] == receita['vencimento']:
+                    
+                    print(mes,receita)
+            
+    print(calendario)
+
+    # print(calendario)
+    
+    # for mes in meses.values():
+    #     print(mes)
+
+
+# verifica se o novo vencimento não tem na lista de contas
+def valida_vencimento(vencimento, lista_contas):
+    
+    for conta in lista_contas:
+        if int(conta["vencimento"]) == int(vencimento):
+            return False
+    return True
+                    
+    
+def elimina_datas_iguais(contas,nome_da_conta):
+
+
+    for conta in contas:
+        
+        vencimento = conta["vencimento"]
+        
+        vencimentos_iguais = [d for d in contas if d["vencimento"] == vencimento and d["vencimento"] != 0]
+        
+        if len(vencimentos_iguais) > 1:
+                        
+            print(f"\nForam encontrados {len(vencimentos_iguais)} {nome_da_conta} com vencimento no dia: {vencimento}\n")
+            print("\nNecessário trocar as datas...")
+            for i, vencimento_igual in enumerate(vencimentos_iguais):
+                
+                index = i + 1 
+                nome = vencimento_igual['nome']
+                print(f"{index}- {nome.capitalize()}")
+            
+            while True:
+                escolha = input(f"\nQual você deseja alterar? (escolha entre 1 e {len(vencimentos_iguais)}): ")
+                
+                if escolha.isdigit() and 0 < int(escolha) <= len(vencimentos_iguais) :
+                    break
+                else:
+                    print("\nEscolha um valor válido") 
+                    
+                        
+            item_escolhido = vencimentos_iguais[int(escolha) - 1]
+            
+            print(f"\nConta escolhida:\n{escolha}- {item_escolhido['nome'].capitalize()}")
+            
+            
+            check = False
+            while check == False:
+                
+                novo_vencimento = input(f"\nQual o novo vencimento: ")
+                resposta = valida_vencimento(novo_vencimento,contas)
+                
+                if novo_vencimento.isdigit() and 0 < int(novo_vencimento) < 31:
+                    
+                    if resposta == True:
+                        for el in contas:
+                            if el["nome"] == item_escolhido["nome"]:
+                                el["vencimento"] = novo_vencimento 
+                                check= True
+                              
+                    else:
+                        print(f"Ja há um vencimento no dia {novo_vencimento} na lista")    
+                    
+               
+                else:
+                    print("\nDigite um numero de 1 a 31")
+            
+                
+     
+def main():
+    
+    # le o excel
+    df_receitas, df_despesas = ler_excel()
+    
+    # organiza os dados
+    dados_receitas = organiza_dados(df_receitas)
+    dados_despesas = organiza_dados(df_despesas)   
+    
+    
+    # verifica se o valor é parcelado e adiciona os meses da parcela nos dados
+    
+    receitas, despesas = verifica_parcela(dados_receitas,dados_despesas)
+    
+    # elimina datas iguais
+    # elimina_datas_iguais(receitas,"Receitas")
+    # elimina_datas_iguais(despesas,"Despesas")
+    
+    linhas = linhas_planilha(receitas,despesas)
+    
+    
+
+main()
 
 # calendario_do_ano()
-verifica_parcela()
+
 # verifica_vencimentos("janeiro")    
